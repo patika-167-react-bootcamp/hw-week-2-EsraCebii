d160f633059b0b11bb0491c2a8b2b11b61308c6b
Esra Cebi <esracebi.81@gmail.com>
2022-02-16 01:08:54 +0300

    yorum satırları kaldı

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// Witout Helper Create user function
-/* function createUser() {
-    const userName = document.getElementById("newUserName").value;
-    const userBalance = document.getElementById("newUserBalance").value;
-    const yeniLi = document.createElement("li"); 
-    yeniLi.className = "list-group-item d-flex justify-content-between align-items-start";
-    const name = document.createElement("div");
-    name.innerText = userName;
-    const balance = document.createElement("span");
-    balance.innerText = userBalance;
-    yeniLi.appendChild(name);
-    yeniLi.appendChild(balance);
-    const list = document.getElementById("user-list");
-    list.appendChild(yeniLi)
-    console.log(userName, userBalance);
-} */
 
 const state = {
     userList: [
